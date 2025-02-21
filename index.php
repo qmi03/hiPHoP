7e13617d4a8e9e96cb1e411e0981dbd4484a08c0
Qmi <minh.pham1126@gmail.com>
2025-02-21 15:13:21 +0700

    chore: update font hello world

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   require("templates/init.php");
 ?>
 
-<p class="bg-red-50">hiPHop</p>
+<p class="bg-blue-50">hiPHop</p>
 
 <?php
   echo "Hello hiPHop";
--- templates/init.php
+++ templates/init.php
@@ -10,5 +10,6 @@
   <meta name="author" content="namdayneee, qmi03, Huy-DNA">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/public/css/tailwind.output.css">
+  <link rel="stylesheet" href="/public/css/fonts.css">
   <script src="/public/js/jquery.js"></script>
 </head>
