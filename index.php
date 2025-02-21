6ff093c2c3feb3748e93f0734927644d6d2e437f
HuyDNA <huydo862003@gmail.com>
2025-02-21 13:38:20 +0700

    chore: empty index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
-require_once 'config/config.php';
-require_once 'config/Database.php';
-require_once 'models/Product.php';
-require_once 'controllers/ProductController.php';
-
-$database = new Database();
-$db = $database->connect();
-
-$productController = new ProductController($db);
-
-$action = $_GET['action'] ?? 'index';
-
-switch($action) {
-    case 'create':
-        $productController->create();
-        break;
-    default:
-        $productController->index();
-        break;
-}
+  echo "Hello hiPHop";
+?>
