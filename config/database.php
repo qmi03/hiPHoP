f052b7ad75c5499bd361319fd21f7d938cfc0cac
HuyDNA <huydo862003@gmail.com>
2025-02-21 18:14:27 +0700

    feat: basic db connection

--- config/database.php
+++ config/database.php
@@ -5,9 +5,4 @@ $password = getenv("DB_PASSWORD");
 $dbname = getenv("DB_NAME");
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-  if ($conn->connect_error) {
-      die("Failed to connect to database" . $conn->connect_error);
-  } else {
-      echo "Conenct successfully";
-  }
 ?>
