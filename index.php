25df5477d667fb918bfa91d54deae37ef3af2734
HuyDNA <huydo862003@gmail.com>
2025-02-21 14:07:29 +0700

    chore: setup tailwindcss

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+  require("templates/init.php")
+?>
+
+<p class="bg-red-50">hiPHop</p>
+
+<?php
   echo "Hello hiPHop";
 ?>
---
+++ templates/init.php
@@ -0,0 +1,9 @@
+<head>
+  <title>hiPHop - The world's largest music ecommerce platform in PHP</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Music ecommerce platform">
+  <meta name="keywords" content="Music, Ecommerce">
+  <meta name="author" content="namdayneee, qmi03, Huy-DNA">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/public/css/tailwind.output.css">
+</head>
