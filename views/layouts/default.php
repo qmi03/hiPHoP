00f388c8582dfe0ea3294e5a1ef6f3c9e92d2144
HuyDNA <huydo862003@gmail.com>
2025-02-25 23:49:04 +0700

    feat: basic header

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -14,6 +14,15 @@
 </head>
 
 <body>
+  <header class="p-8 flex justify-between">
+    <a role="banner" href="/" class="font-black text-6xl tracking-widest">hiPHoP</a>
+    <button class="font-extralight text-xl flex gap-1">
+      <span>MENU</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
+    </button>
+  </header>
   <?php print $content ?>
 </body>
 
