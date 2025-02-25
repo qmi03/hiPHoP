c6216ddd113132d4afc3df3e93ad527f76b873a5
HuyDNA <huydo862003@gmail.com>
2025-02-26 05:02:23 +0700

    feat: add nav bar on large screens

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -18,15 +18,27 @@ $path = $_SERVER["REQUEST_URI"];
 ?>
 
 <body>
-  <header class="p-8 flex justify-between items-center">
-    <a role="banner" href="/" class="font-black text-3xl tracking-widest">hiPHoP</a>
+  <header>
+    <div class="p-8 flex justify-between items-center">
+      <a role="banner" href="/" class="font-black text-3xl tracking-widest">hiPHoP</a>
+      <div>
+        <button onclick="$('#sidemenu').removeClass('translate-x-full')" class="lg:hidden font-extralight text-xl flex gap-1 items-center cursor-pointer">
+          <span>MENU</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div>
+    </div>
     <div>
-      <button onclick="$('#sidemenu').removeClass('translate-x-full')" class="lg:hidden font-extralight text-xl flex gap-1 items-center cursor-pointer">
-        <span>MENU</span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-      </button>
+      <nav class="hidden lg:flex justify-around text-white bg-[#111] py-4 text-lg">
+        <a href="/">HOME</a>
+        <a href="/shop">SHOP</a>
+        <a href="/blog">BLOG</a>
+        <a href="/about">ABOUT US</a>
+        <a href="/contact">CONTACT US</a>
+        <a href="/faq">FAQ</a>
+      </nav>
       <nav id="sidemenu" class="translate-x-full transition-transform transition-duration-[2s] lg:hidden bg-[#111] fixed top-0 right-0 pl-18 pr-32 pt-28 h-[100vh]">
         <button onclick="$('#sidemenu').addClass('translate-x-full')" class="text-white absolute top-10 right-10 cursor-pointer transition-transform">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -50,14 +62,12 @@ $path = $_SERVER["REQUEST_URI"];
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 30" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="m7.25 4.5 7.5 7.5-7.5 7.5" />
           </svg>
-
         </a>
         <a class="flex gap-2 items-center text-xl <?= $path == "/about" ? "text-white" : "text-gray-400" ?> m-4" href="/about">
           <span>ABOUT US</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 30" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="m7.25 4.5 7.5 7.5-7.5 7.5" />
           </svg>
-
         </a>
         <a class="flex gap-2 items-center text-xl <?= $path == "/contact" ? "text-white" : "text-gray-400" ?> m-4" href="/contact">
           <span>CONTACT US</span>
