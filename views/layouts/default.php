834ad08b8d2ae3f04283a7c43bc7f8d93d98acef
HuyDNA <huydo862003@gmail.com>
2025-02-27 00:08:33 +0700

    feat: add cart, login and search buttons

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -20,8 +20,27 @@ $path = $_SERVER["REQUEST_URI"];
 <body>
   <header>
     <div class="p-8 flex justify-between items-center">
-      <a role="banner" href="/" class="font-black text-3xl tracking-widest">hiPHoP</a>
-      <div>
+      <a role="banner" href="/" class="font-black text-3xl tracking-widest grow-1">hiPHoP</a>
+      <div class="flex items-center justify-between grow-6 lg:justify-end">
+        <div class="flex gap-10">
+          <button class="flex gap-2 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+            <span>CART</span>
+          </button>
+          <button class="flex gap-2 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+            <span>LOGIN</span>
+          </button>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </div>
         <button onclick="$('#sidemenu').removeClass('translate-x-full')" class="lg:hidden font-extralight text-xl flex gap-1 items-center cursor-pointer">
           <span>MENU</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
