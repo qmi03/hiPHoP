e40a8af811feaa3ece234fb5016d83650f0625bc
HuyDNA <huydo862003@gmail.com>
2025-03-06 14:25:57 +0700

    fix: newsletter bg responsiveness

--- views/home/index.php
+++ views/home/index.php
@@ -1,12 +1,12 @@
-<section class="flex text-white">
-  <div class="flex-3/4 bg-[url(https://musicplace.com/images/Facilities/DadTeachesSon692x900.2.jpg)] bg-no-repeat bg-cover bg-center relative">
+<section class="md:flex text-white">
+  <div class="min-h-[500px] md:min-h-0 md:flex-3/4 bg-[url(https://musicplace.com/images/Facilities/DadTeachesSon692x900.2.jpg)] bg-no-repeat bg-cover bg-center relative">
     <div class="absolute bottom-10">
       <h2 class="text-3xl bg-[#11111155] px-8 pt-8">Vocal</h2>
       <p class="text-xl bg-[#11111155] px-8 pb-8">AGES 4 & UP: In "All Arts" (Vocal/Drama) classes, Vocal Ensembles and Private Lessons, singers develop skills in technique & performance. All styles and genres are offered and supported by frequent performance opportunities.</p>
       <a class="inline-block bg-cyan-600 mx-8 px-4 py-2 text-lg font-bold my-6">LEARN MORE</a>
     </div>
   </div>
-  <div class="flex-1/4 flex flex-col gap-0">
+  <div class="md:flex-1/4 flex flex-col gap-0">
     <button class="h-24 cursor-pointer bg-pink-500 flex gap-2 items-center p-2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
