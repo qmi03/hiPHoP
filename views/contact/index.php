04db9ed54f1cca3b8437921f79ed5941272cedf6
HuyDNA <huydo862003@gmail.com>
2025-03-07 15:26:14 +0700

    fix: remove whitespaces in textarea

--- views/contact/index.php
+++ views/contact/index.php
@@ -18,8 +18,7 @@
     <label class="text-left lg:text-right text-pink-600 lg:self-center cursor-pointer" for="title">Title *</label>
     <input type="text" required class="px-2 py-1 border-gray-300 border-2 rounded-sm focus:border-pink-200 shadow-gray shadow-sm text-lg italic" id="title">
     <label class="mt-5 text-left lg:text-right text-pink-600 lg:self-center cursor-pointer" for="content">Content *</label>
-    <textarea required class="px-2 py-1 border-gray-300 border-2 rounded-sm focus:border-pink-200 shadow-gray shadow-sm text-lg italic" id="content">
-    </textarea>
+    <textarea required class="px-2 py-1 border-gray-300 border-2 rounded-sm focus:border-pink-200 shadow-gray shadow-sm text-lg italic" id="content"></textarea>
     <div></div>
     <div class="mt-5 lg:mt-0 flex items-center justify-start lg:none">
       <input type="submit" value="SEND" class="cursor-pointer text-white text-lg font-bold px-3 py-2 bg-cyan-600 hover:bg-gray-500 transition-colors">
