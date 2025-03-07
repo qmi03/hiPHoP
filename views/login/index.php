1a59c20f34214da44c7f1516de1a4ce9cb231d12
HuyDNA <huydo862003@gmail.com>
2025-03-07 14:03:15 +0700

    fix: improve login UX

--- views/login/index.php
+++ views/login/index.php
@@ -3,20 +3,20 @@
 </section>
 <section class="bg-white mx-5 lg:mx-20 p-10 relative top-[-10px]">
   <form action="" method="post" class="flex flex-col lg:grid grid-cols-[200px_1fr] gap-x-5 lg:gap-y-5 text-xl">
-    <label class="text-left lg:text-right text-pink-600 lg:self-center" for="email">Email *</label>
+    <label class="text-left lg:text-right text-pink-600 lg:self-center cursor-pointer" for="email">Email *</label>
     <input type="email" required class="px-2 py-1 border-gray-300 border-2 rounded-sm focus:border-pink-200 shadow-gray shadow-sm text-lg italic" id="email">
-    <label class="mt-5 lg:mt-0 text-left lg:text-right text-pink-600 lg:self-center" for="password">Password *</label>
+    <label class="mt-5 lg:mt-0 text-left lg:text-right text-pink-600 lg:self-center cursor-pointer" for="password">Password *</label>
     <input class="px-2 py-1 border-gray-300 border-2 rounded-sm focus:border-pink-200 shadow-gray shadow-sm text-lg" type="password" required id="password" minlength="6" maxlength="256">
     <div class="mt-5 lg:mt-0 flex gap-5 lg:justify-end">
-      <label class="text-left lg:text-right" for="remember-me-checked">Remember me</label>
-      <input type="checkbox" id="remember-me-checked" class="lg:hidden">
+      <label class="text-left lg:text-right cursor-pointer" for="remember-me-checked">Remember me</label>
+      <input type="checkbox" id="remember-me-checked" class="lg:hidden cursor-pointer">
     </div>
-    <div class="hidden lg:flex items-center justify-start">
+    <div class="hidden lg:flex items-center justify-start cursor-pointer">
       <input type="checkbox" id="remember-me-checked">
     </div>
     <div></div>
     <div class="mt-5 lg:mt-0 flex items-center justify-start lg:none">
-      <input type="submit" value="LOG IN" class="text-white text-lg font-bold px-3 py-2 bg-cyan-600">
+      <input type="submit" value="LOG IN" class="cursor-pointer text-white text-lg font-bold px-3 py-2 bg-cyan-600 hover:bg-gray-500 transition-colors">
     </div>
   </form>
 
@@ -25,7 +25,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
       </svg>
-      <span class="text-lg">Forgot your password?</span>
+      <span class="text-lg hover:text-black transition-colors duration-300">Forgot your password?</span>
     </a>
   </div>
 </section>
