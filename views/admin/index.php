75d83ff1b5573a652f36e62d8e8c6f52dec473eb
HuyDNA <huydo862003@gmail.com>
2025-03-12 17:07:35 +0700

    feat: basic photo uplaoder form

--- views/admin/index.php
+++ views/admin/index.php
@@ -74,43 +74,75 @@
       </div>
     </div>
   </section>
+  <script>
+    let currentPage;
+    updateCurrentPage(<?= htmlspecialchars($data["currentPhotoPage"]) ?>);
+
+    function firstPage() {
+      return Math.floor(currentPage / 3) * 3;
+    }
+
+    function updateCurrentPage(page) {
+      currentPage = page;
+      $("#page-item-1").children("a").first().text(firstPage() + 1).attr("href", `/admin?gallery-page=${firstPage()}`).removeClass("active");
+      $("#page-item-2").children("a").first().text(firstPage() + 2).attr("href", `/admin?gallery-page=${firstPage() + 1}`).removeClass("active");
+      $("#page-item-3").children("a").first().text(firstPage() + 3).attr("href", `/admin?gallery-page=${firstPage() + 2}`).removeClass("active");
+      if (firstPage() === currentPage) {
+        $("#page-item-1").children("a").first().addClass("active");
+      } else if (firstPage() + 1 === currentPage) {
+        $("#page-item-2").children("a").first().addClass("active");
+      } else if (firstPage() + 2 === currentPage) {
+        $("#page-item-3").children("a").first().addClass("active");
+      }
+    }
+
+    $("#page-item-left").on("click", () => {
+      if (currentPage <= 2) {
+        return;
+      }
+      currentPage -= 3;
+      updateCurrentPage(firstPage());
+    })
+
+    $("#page-item-right").on("click", () => {
+      if (Math.floor(currentPage / 3) >= Math.floor(<?= $data["totalPhotoPages"] ?> / 3)) {
+        return;
+      }
+      currentPage += 3;
+      updateCurrentPage(firstPage());
+    })
+  </script>
+
+  <section class="card">
+    <div class="card-header">
+      <h5 class="card-title">Photo uploader</h5>
+    </div>
+    <div class="card-body mt-0">
+      <form method="POST" action="/admin/photo">
+        <div class="mb-3">
+          <label class="form-label cursor-pointer" for="image-name">Name</label>
+          <input type="text" class="form-control" name="name" id="image-name">
+        </div>
+        <div class="mb-3">
+          <!-- File uploader with image preview -->
+          <div class="filepond--root image-preview-filepond filepond--hopper" data-style-button-remove-item-position="left" data-style-button-process-item-position="right" data-style-load-indicator-position="right" data-style-progress-indicator-position="right" data-style-button-remove-item-align="false" style="height: 76px;"><input class="filepond--browser" type="file" id="filepond--browser-mr225r250" name="filepond" aria-controls="filepond--assistant-mr225r250" aria-labelledby="filepond--drop-label-mr225r250" accept="image/png,image/jpg,image/jpeg">
+            <div class="filepond--drop-label" style="transform: translate3d(0px, 0px, 0px); opacity: 1;"><label for="filepond--browser-mr225r250" id="filepond--drop-label-mr225r250" aria-hidden="true">Drag &amp; Drop your files or <span class="filepond--label-action" tabindex="0">Browse</span></label></div>
+            <div class="filepond--list-scroller" style="transform: translate3d(0px, 0px, 0px);">
+              <ul class="filepond--list" role="list"></ul>
+            </div>
+            <div class="filepond--panel filepond--panel-root" data-scalable="true">
+              <div class="filepond--panel-top filepond--panel-root"></div>
+              <div class="filepond--panel-center filepond--panel-root" style="transform: translate3d(0px, 8px, 0px) scale3d(1, 0.6, 1);"></div>
+              <div class="filepond--panel-bottom filepond--panel-root" style="transform: translate3d(0px, 68px, 0px);"></div>
+            </div><span class="filepond--assistant" id="filepond--assistant-mr225r250" role="status" aria-live="polite" aria-relevant="additions"></span>
+            <fieldset class="filepond--data"></fieldset>
+            <div class="filepond--drip"></div>
+          </div>
+        </div>
+        <div>
+          <input type="submit" class="btn btn-primary mb-3" value="Upload">
+        </div>
+      </form>
+    </div>
+  </section>
 </div>
-
-<script>
-  let currentPage;
-  updateCurrentPage(<?= htmlspecialchars($data["currentPhotoPage"]) ?>);
-
-  function firstPage() {
-    return Math.floor(currentPage / 3) * 3;
-  }
-
-  function updateCurrentPage(page) {
-    currentPage = page;
-    $("#page-item-1").children("a").first().text(firstPage() + 1).attr("href", `/admin?gallery-page=${firstPage()}`).removeClass("active");
-    $("#page-item-2").children("a").first().text(firstPage() + 2).attr("href", `/admin?gallery-page=${firstPage() + 1}`).removeClass("active");
-    $("#page-item-3").children("a").first().text(firstPage() + 3).attr("href", `/admin?gallery-page=${firstPage() + 2}`).removeClass("active");
-    if (firstPage() === currentPage) {
-      $("#page-item-1").children("a").first().addClass("active");
-    } else if (firstPage() + 1 === currentPage) {
-      $("#page-item-2").children("a").first().addClass("active");
-    } else if (firstPage() + 2 === currentPage) {
-      $("#page-item-3").children("a").first().addClass("active");
-    }
-  }
-
-  $("#page-item-left").on("click", () => {
-    if (currentPage <= 2) {
-      return;
-    }
-    currentPage -= 3;
-    updateCurrentPage(firstPage());
-  })
-
-  $("#page-item-right").on("click", () => {
-    if (Math.floor(currentPage / 3) >= Math.floor(<?= $data["totalPhotoPages"] ?> / 3)) {
-      return;
-    }
-    currentPage += 3;
-    updateCurrentPage(firstPage());
-  })
-</script>
--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="/public/mazer/compiled/css/app.css">
   <link rel="stylesheet" href="/public/mazer/compiled/css/app-dark.css">
   <link rel="stylesheet" href="/public/mazer/compiled/css/iconly.css">
+  <link rel="stylesheet" href="/public/mazer/extensions/filepond/filepond.css">
+  <link rel="stylesheet" href="/public/mazer/extensions/filepond-plugin-image-preview/filepond-plugin-image-preview.css">
+  <link rel="stylesheet" href="/public/mazer/extensions/toastify-js/src/toastify.css">
   <script src="/public/mazer/static/js/initTheme.js"></script>
   <script src="/public/js/jquery.js"></script>
 </head>
@@ -201,5 +204,15 @@ array_pop($subpaths);
 <script src="public/mazer/static/js/components/dark.js"></script>
 <script src="public/mazer/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="public/mazer/compiled/js/app.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-image-preview/filepond-plugin-image-preview.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-image-crop/filepond-plugin-image-crop.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-image-exif-orientation/filepond-plugin-image-exif-orientation.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-image-filter/filepond-plugin-image-filter.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-image-resize/filepond-plugin-image-resize.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-file-validate-size/filepond-plugin-file-validate-size.min.js"></script>
+<script src="public/mazer/extensions/filepond-plugin-file-validate-type/filepond-plugin-file-validate-type.min.js"></script>
+<script src="public/mazer/extensions/filepond/filepond.js"></script>
+<script src="public/mazer/extensions/toastify-js/src/toastify.js"></script>
+<script src="public/mazer/static/js/pages/filepond.js"></script>
 
 </html>
