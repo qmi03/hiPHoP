660c3f6d7540226760748e397875dbd53fa74721
HuyDNA <huydo862003@gmail.com>
2025-03-28 09:40:17 +0700

    feat: put correct admin avatar

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -45,7 +45,7 @@ array_pop($subpaths);
               <div class="dropdown">
                 <a href="#" id="topbarUserDropdown" class="user-dropdown d-flex align-items-center dropend dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
                   <div class="avatar avatar-md2">
-                    <img src="/public/mazer/compiled/jpg/1.jpg" alt="Avatar">
+                    <img src="<?php echo $GLOBALS['user']->avatarUrl; ?>" alt="Avatar">
                   </div>
                   <div class="text">
                     <h6 class="user-dropdown-name"><?php echo htmlspecialchars($data['user']->username); ?></h6>
