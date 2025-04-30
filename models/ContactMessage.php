3a9e77f6b36f151a526c4b5de86b7a27862f9b69
Huy-DNA <huydo862003@gmail.com>
2025-04-30 12:48:54 +0000

    feat: create and response to contact message

--- models/ContactMessage.php
+++ models/ContactMessage.php
@@ -110,4 +110,101 @@ class ContactMessageModel
       return 0;
     }
   }
+
+  public function fetchPageByUserId(int $userId, int $pageNumber, int $pageSize): array
+  {
+    $conn = Database::getInstance();
+    try {
+      $conn->beginTransaction();
+      $offset = $pageNumber * $pageSize;
+      $stmt = $conn->prepare('
+        SELECT cm.*, u.username
+        FROM contact_messages cm
+        LEFT JOIN users u
+        ON cm.user_id = u.id
+        WHERE cm.user_id = :userId
+        ORDER BY cm.id DESC
+        LIMIT :limit
+        OFFSET :offset
+      ');
+      $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+      $stmt->bindValue(':limit', $pageSize, PDO::PARAM_INT);
+      $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+      $stmt->execute();
+      $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $conn->commit();
+
+      return array_map(fn($message) => new ContactMessage(
+        id: $message['id'],
+        userId: $message['user_id'],
+        username: $message['username'],
+        message: $message['message'],
+        response: $message['response'],
+        respondedAt: new DateTime($message['responded_at'])
+      ), $messages);
+    } catch (PDOException $e) {
+      $conn->rollBack();
+      return [];
+    }
+  }
+  public function countByUserId(int $userId): int
+  {
+    $conn = Database::getInstance();
+    try {
+      $conn->beginTransaction();
+      $stmt = $conn->prepare('SELECT COUNT(*) FROM contact_messages where user_id = :userId');
+      $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+      $stmt->execute();
+      $count = $stmt->fetchColumn();
+      $conn->commit();
+
+      return (int)$count;
+    } catch (PDOException $e) {
+      return 0;
+    }
+  }
+
+  public function create(int $userId, string $message): bool
+  {
+    $conn = Database::getInstance();
+    try {
+      $conn->beginTransaction();
+      $stmt = $conn->prepare('
+        INSERT INTO contact_messages (user_id, message)
+        VALUES (:userId, :message)
+      ');
+      $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+      $stmt->bindValue(':message', $message, PDO::PARAM_STR);
+      $stmt->execute();
+      $conn->commit();
+
+      return true;
+    } catch (PDOException $e) {
+      $conn->rollBack();
+      return false;
+    }
+  }
+
+  public function response(int $id, string $response): bool
+  {
+    $conn = Database::getInstance();
+    try {
+      $conn->beginTransaction();
+      $stmt = $conn->prepare('
+        UPDATE contact_messages
+        SET response = :response, responded_at = NOW()
+        WHERE id = :id
+      ');
+      $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+      $stmt->bindValue(':response', $response, PDO::PARAM_STR);
+      $stmt->execute();
+      $conn->commit();
+
+      return true;
+    } catch (PDOException $e) {
+      $conn->rollBack();
+      return false;
+    }
+  }
 }
+
